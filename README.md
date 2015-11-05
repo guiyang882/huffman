0dfdcfffb1bd06c8dfa386418d587ed5decfd871
Liu Guiyang <liuguiyangnwpu@163.com>
2015-11-05 19:13:02 +0800

    add some Usage README.md

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 ## usage example:
     python compress.py result/Lorem_ipsum.txt result/a.bin
     python decompress.py result/a.bin result/decode_test.txt
+
+## Use voilence search method to decode the file
+	time python decompress.py result/a.bin result/decode_test.txt 
+	Decode Finished !
+
+	real	1m6.263s
+	user	1m5.760s
+	sys	0m0.440s
+
+## Use Prefix search method to decode the file
+	time python decompress.py result/a.bin result/decode_test.txt 
+	Decode Finished !
+
+	real	15m14.708s
+	user	14m50.864s
+	sys	0m8.756s
+
--- decompress.py
+++ decompress.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pickle
 import struct
+import copy
 #from include.Node import Node
 
 class TreeNode:
@@ -12,7 +13,10 @@ class TreeNode:
         self.right = right
 
     def __str__(self):
-        return "Node key %s, left %s, right %s" % (str(self.key),str(self.left),str(self.right))
+        #return "key %s, left %s, right %s" % (str(self.key),str(self.left),str(self.right))
+        if self.key != None:
+            return "Key %s" % self.key
+        return ""
 
     def setkey(self,key):
         self.key = key
@@ -29,35 +33,30 @@ class Tree:
     def getRoot(self):
         return self.root
 
-    def insert(self,node,code,key):
-        if len(code) == 1:
-            print "prepare insert "
-            print code
-            if code[0] == '0' and node.left == None:
+    def insert(self,node,code,key,index):
+        #print repr(code),len(code)
+        if index == len(code)-1:
+            if code[index] == '0' and node.left == None:
                 node.left = TreeNode("",key)
-                print "Here 1",node.left
-            elif code[0] == '0' and node.left != None:
+            elif code[index] == '0' and node.left != None:
                 node.left.setkey(key)
-                print "Here 2",node.left
 
-            if code[0] == '1' and node.right == None:
-                node.left = TreeNode("",key)
-                print "Here 3",node.right
-            elif code[0] == '1' and node.right != None:
+            if code[index] == '1' and node.right == None:
+                node.right = TreeNode("",key)
+            elif code[index] == '1' and node.right != None:
                 node.right.setkey(key)
-                print "Here 4",node.right
             return True
-        c = code[0]
-        newcode = code[1:]
-        print newcode
+
+        c = code[index]
+        index = index + 1
         if c == '0':
             if node.left == None:
                 node.left = TreeNode("")
-            self.insert(node.left,key,newcode)
-        if c == '1':
+            self.insert(node.left,code,key,index)
+        elif c == '1':
             if node.right == None:
                 node.right = TreeNode("")
-            self.insert(node.right,key,newcode)
+            self.insert(node.right,code,key,index)
 
     def preOrderTree(self,root):
         if root != None:
@@ -66,24 +65,32 @@ class Tree:
             self.preOrderTree(root.left)
         if root.right != None:
             self.preOrderTree(root.right)
+    
+    def combine_key_map(self,root,code):
+        if root != None and root.left == None and root.right == None:
+            print root.key,code
+        if root.left != None:
+            self.combine_key_map(root.left,code + "0")
+        if root.right != None:
+            self.combine_key_map(root.right,code + "1")
+
 
 def create_Huffman_tree(code_map):
     info = code_map.items()
     info.sort(key = lambda x:len(x[1]))
     TreeObj = Tree()
-
     for item in info:
-        print item[1],item[0]
-        TreeObj.insert(TreeObj.getRoot(),item[1],item[0])
-        sys.exit(0)
-    TreeObj.preOrderTree(TreeObj.getRoot())
+        TreeObj.insert(TreeObj.getRoot(),item[1],item[0],0)
+    root = TreeObj.getRoot()
+    #TreeObj.combine_key_map(root,"")
+    return root
 
 class decompress:
     def __init__(self,pickle_file,compressedfile,targetFile):
         handle = open(pickle_file,'rb')
         self.key_map = pickle.load(handle)
         handle.close()
-        create_Huffman_tree(self.key_map)
+        self.root = create_Huffman_tree(self.key_map)
         #print self.key_map
         self.code_content = []
         #print compressedfile
@@ -103,6 +110,7 @@ class decompress:
         self.decode_content = "".join(seg_list)
         self.str_content = ""
         self.write_dest = targetFile
+
     def decode(self):
         index = 0
         total_length = len(self.decode_content)
@@ -128,6 +136,40 @@ class decompress:
         handle.write(self.str_content)
         handle.close()
         print "Decode Finished !"
+
+    def tree_decode(self):
+        page_content = ""
+        current = copy.deepcopy(self.root)
+        for index in range(0,len(self.decode_content)):
+            if self.decode_content[index] == '0':
+                current = current.left
+                if current != None and current.key != None:
+                    if current.key != '\xff\xff':
+                        page_content = page_content + current.key
+                        current = copy.deepcopy(self.root)
+                    if current.key == '\xff\xff':
+                        break
+                if current == None:
+                    print "Tree or Code Error !"
+                    raise TypeError
+
+            elif self.decode_content[index] == '1':
+                current = current.right
+                if current != None and current.key != None:
+                    if current.key != '\xff\xff':
+                        page_content = page_content + current.key
+                        current = copy.deepcopy(self.root)
+                    if current.key == '\xff\xff':
+                        break
+                if current == None:
+                    print "Tree or Code Error !"
+                    raise TypeError
+
+        handle = open(self.write_dest,"w")
+        handle.write(page_content)
+        handle.close()
+        print "Use Prefix to decode the File Finished !"
+
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print 'Usage: python decompress.py compressedFile.bin test.txt'
@@ -137,3 +179,4 @@ if __name__ == '__main__':
         sys.exit(0)
     obj = decompress("./result/key.pkl","./result/a.bin",sys.argv[2])
     #obj.decode()
+    obj.tree_decode()
